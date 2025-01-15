3265413c75f47135341ad9b527ad78ea79ef14f1
Rayan <139246019+Rayanzin@users.noreply.github.com>
2025-01-15 12:45:42 -0300

    header  pronto, falta css

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+const Header = () => {
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li>
+                        <img src="" alt="Logo" />
+                    </li>
+                    <li>
+                        <a href="/">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="/NossoCafe">Nosso Café</a>
+                    </li>
+                    <li>
+                        <a href="/Noticias">Notícias</a>
+                    </li>
+                    <li>
+                        <a href="/Contato">Contato</a>
+                    </li>
+                    <li>
+                        <a href="/Cardapio">Cardápio</a>
+                    </li>
+                </ul>
+                <div>
+                    <a href="/franquia">Seja um Franqueado</a>
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default Header;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+    return ( 
+        <>
+            <h1>Página não encontrada</h1>
+            <a href="/">Voltar ao Inicio</a>
+        </>
+     );
+}
+ 
+export default NotFound;
