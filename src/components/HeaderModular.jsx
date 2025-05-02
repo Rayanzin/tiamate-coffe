3268a380f3e6bbde50972dec5ad6a142b5033ea0
Rayan <139246019+Rayanzin@users.noreply.github.com>
2025-05-02 19:57:33 -0300

    up

--- src/components/HeaderModular.jsx
+++ src/components/HeaderModular.jsx
@@ -16,13 +16,13 @@ const HeaderModular = ({
   // 2xl -> 1535px
 
   return (
-    <section className="relative m-auto w-full h-[490px] max-lg:h-[400px] duration-200 max-md:h-[300px] max-sm:h-[250px]">
+    <section className="relative m-auto w-full h-[490px] max-lg:h-[400px] duration-200 max-md:h-[300px] max-sm:h-[250px] max-[550px]:h-[200px]">
       <div className="absolute z-0 w-full h-full">
         <img className="w-full h-full " src={imagem} alt="café" />
       </div>
       <div className="relative h-full m-auto">
         <div
-          className={`z-10 relative top-[110px] !max-w-[1170px] m-auto ${className} max-md:top-[50px] max-sm:top-[50px]`}
+          className={`z-10 relative top-[110px] !max-w-[1170px] m-auto ${className} max-md:top-[50px] max-sm:top-[50px] max-[550px]:top-[25px]`}
         >
           <div className={`flex flex-col gap-[14px] w-[570px] ${classH2} max-[1190px]:px-[10px] max-lg:w-[400px] max-lg:ml-[10%] max-sm:ml-[50px] max-sm:w-[300px]`}>
             <h2 className={`text-letras text-[55px] leading-none font-bold max-lg:text-[40px] max-sm:text-[30px]`}>
